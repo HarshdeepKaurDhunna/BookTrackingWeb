97204efd74eefd0b3e816ace80a02d552c2af980
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-08-11 22:52:32 -0400

    Add Database configurations, settings and migrations set it to common library

--- BookTrackingWeb/Startup.cs
+++ BookTrackingWeb/Startup.cs
@@ -1,6 +1,8 @@
+using BookTrackingWebData.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,22 @@ namespace BookTrackingWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //For Windows Configerations
+            string dbConnectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<BookTrackingWebContext>(options =>
+            options
+             .UseSqlServer(dbConnectionString,
+                 b => b.MigrationsAssembly("BookTrackingWebData"))
+             );
+
+            //For Mac Configerations
+            /* services.AddDbContext<BookTrackingWebContext>(options =>
+             options
+              .UseSqlite(dbConnectionString,
+                  b => b.MigrationsAssembly("BookTrackingWebData"))
+              );*/
+
             services.AddRazorPages();
         }
 
