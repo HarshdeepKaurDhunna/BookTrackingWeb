536eb856dd69605400a0b77d8071384a1a19ef1d
savithamk <savitha.maiya1@gmail.com>
2022-08-11 23:35:05 -0400

    Created the controller for Home view
    Added the web page view in the home controller to show home page

---
+++ BookTrackingWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using BookTrackingWebData.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTrackingWebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly BookTrackingWebContext _db;
+
+        public HomeController(ILogger<HomeController> logger, BookTrackingWebContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
