5136660385ae4f4b072f63fcf92fc81562f7e643
savithamk <savitha.maiya1@gmail.com>
2022-08-18 14:05:45 -0400

    Created controllers for BookQuote and BookReadTrack

---
+++ BookTrackingWebAPI/Controllers/BookQuoteController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BookTrackingWebData.Data;
+using BookTrackingWebLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BookTrackingWebAPI.Controllers
+{
+    public class BookQuoteController
+    {
+        private readonly ILogger<BookQuoteController> _logger;
+
+        private readonly BookTrackingWebContext _db;
+
+
+        public BookQuoteController(ILogger<CategoryController> logger, BookTrackingWebContext db)
+        {
+            _logger = (ILogger<BookQuoteController>)logger;
+            _db = db;
+        }
+    }
+}
