057f67b7a4c4ad54ed536750e34477cad58c6d67
savithamk <savitha.maiya1@gmail.com>
2022-08-18 18:58:53 -0400

    Added endpoint for saving book tracking details calculated from and to book pages read by using totalPagesRead

--- BookTrackingWebAPI/Controllers/BookReadTrackController.cs
+++ BookTrackingWebAPI/Controllers/BookReadTrackController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using BookTrackingWebData.Data;
+using BookTrackingWebLibrary;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -16,6 +19,39 @@ namespace BookTrackingWebAPI.Controllers
         {
             _logger = (ILogger<BookReadTrackController>)logger;
             _db = db;
+        }
+
+        [HttpPost("bookTrack")]
+        public ActionResult saveBookTrackingDetails(int bookId, int totalPagesRead)
+        {
+            try
+            {
+                if (bookId > 0 || totalPagesRead > 0)
+                {
+                    BookReadTrack bookReadTrack = new BookReadTrack();
+                    bookReadTrack.BookId = bookId;
+                    bookReadTrack.BookReadDate = DateTime.Now;
+
+                    BookReadTrack lastTrackingEntryForBook = _db.BookReadTracks.Where(track => track.BookId == bookId).OrderByDescending(track => track.BookReadDate).First();
+
+                    bookReadTrack.BookTotalPagesRead = totalPagesRead;
+
+                    bookReadTrack.BookFromPage = lastTrackingEntryForBook.BookToPage;
+                    bookReadTrack.BookToPage = lastTrackingEntryForBook.BookToPage + totalPagesRead;
+
+                    _db.Add(bookReadTrack);
+                    _db.SaveChangesAsync();
+                    return Ok(bookReadTrack);
+                }
+                else
+                {
+                    return BadRequest("Invalid input data in the request");
+                }
+            }
+            catch
+            {
+                return BadRequest("Invalid input data in the request");
+            }
         }
     }
 }
