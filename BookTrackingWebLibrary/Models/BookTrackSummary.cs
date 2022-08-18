897d4ab45594d13b85de6d2ad3804d0d8f51b8ab
savithamk <savitha.maiya1@gmail.com>
2022-08-18 19:17:00 -0400

    Added model class for displaying book tracking summary data and added total book pages field in book model.

---
+++ BookTrackingWebLibrary/Models/BookTrackSummary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BookTrackingWebLibrary.Models
+{
+    public class BookTrackSummary
+    {
+        public int BookId { get; set; } //Id for books
+
+        public string BookISBN { get; set; } //ISBN of book
+
+        public string BookTitle { get; set; } //Book title 
+
+        public int TotalPages { get; set; } // Book Total Pages
+
+        public DateTime LastReadOn { get; set; } // last read date
+
+        public int RemainingPages { get; set; }// remaining pages to read
+    }
+}
