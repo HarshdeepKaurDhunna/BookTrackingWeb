87e9eab43cf5ef04df477602033e8346d1cdbbde
savithamk <savitha.maiya1@gmail.com>
2022-08-18 18:48:55 -0400

    Added endpoint for saving book tracking details
    calculated from and to book pages read by using totalPagesRead

--- BookTrackingWebAPI/Controllers/BookReadTrackController.cs
+++ BookTrackingWebAPI/Controllers/BookReadTrackController.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using BookTrackingWebData.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 namespace BookTrackingWebAPI.Controllers
 {
-    public class BookReadTrackController
+    public class BookReadTrackController: Controller
     {
-        private readonly ILogger<BookQuoteController> _logger;
+        private readonly ILogger<BookReadTrackController> _logger;
 
         private readonly BookTrackingWebContext _db;
 
 
-        public BookQuoteController(ILogger<CategoryController> logger, BookTrackingWebContext db)
+        public BookReadTrackController(ILogger<BookReadTrackController> logger, BookTrackingWebContext db)
         {
-            _logger = (ILogger<BookQuoteController>)logger;
+            _logger = (ILogger<BookReadTrackController>)logger;
             _db = db;
         }
     }
