3e62c182c5623362e5fb50b69bbb27708b7bef5c
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-08-18 20:38:53 -0400

    Dynamically add labels and data to line chart

--- BookTrackingWeb/Pages/Index.cshtml.cs
+++ BookTrackingWeb/Pages/Index.cshtml.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -19,24 +21,51 @@ namespace BookTrackingWeb.Pages
         public int wantReadBooks = 0;
         public int currentlyReadBooks = 0;
         public int readBooks = 0;
-        public string[] Labels { get; set; }
-        public string Legend { get; set; }
-        public int[] Data { get; set; }
+        public string[] Labels;
+        public int[] Data;
         public IList<BookReadTrack> BookTrackList { get; set; }
         public IndexModel(BookTrackingWebContext context)
         {
             _context = context;
         }
        
+        /**
+         * @Method popukate data on page load
+         */
         public void OnGet()
         {
-            List<Book> BooksList = _context.Books.ToList();
+           
+            //BookReadTrack list from Db 
             List<BookReadTrack> BookTrackList = _context.BookReadTracks.Include(b => b.Book).ToList();
-                         
+
+            //Filter BookTrackList to get bookreadDate and pageCount for chart.js according to status
+            List<string> LabelList = BookTrackList.Select(item => convertDateToString(item.BookReadDate)).ToList();
+            List<int> DataList = BookTrackList.Select(item => getBookPages(item.BookToPage, item.BookFromPage)).ToList();
+
+            //Convert List to array to populate in chart
+            Labels = LabelList.ToArray();
+            Data = DataList.ToArray();
+
+            //book list from Db 
+            List<Book> BooksList = _context.Books.ToList();
+
+            //Filter BooksList to get count of books according to status
             totalBooks = BooksList.Count;
             wantReadBooks = BooksList.Count(x => x.BookStatus == "1");
             currentlyReadBooks = BooksList.Count(x => x.BookStatus == "2");
             readBooks = BooksList.Count(x => x.BookStatus == "3");
+        }
+
+        //Method to get page count
+        public int getBookPages(int endPage, int startPage)
+        {
+            return endPage - startPage;
+        }
+
+        //Method to format date
+        public string convertDateToString(DateTime dateVal)
+        {
+            return dateVal.ToString("MMMM dd, yyyy"); ; ;
         }
     }
 }
