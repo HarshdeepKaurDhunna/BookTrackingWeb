27ed968c68bd2b40a9509839f3eabb15e5bccceb
savithamk <savitha.maiya1@gmail.com>
2022-08-18 21:50:28 -0400

    Added integration tests for all controllers

---
+++ BookTrackingWeb-Test/ApiIntegrationTestsForControllers.cs
@@ -0,0 +1,83 @@
+﻿using BookTrackingWebAPI;
+using System.Net.Http;
+using Xunit;
+namespace Book_Tracking_Application_Test
+{
+    public class ApiIntegrationTestsForControlleres : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public ApiIntegrationTestsForControlleres(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+
+        [Fact]
+        public async void fetchCategories_ReturnsAllCategories()
+        {
+            // Arrange
+            var request = "categories";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async void fetchCategoryTypes_ReturnsAllCategoryTypes()
+        {
+            // Arrange
+            var request = "categoryTypes";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+
+        [Fact]
+        public async void fetchBooks_ReturnsAllBooks()
+        {
+            // Arrange
+            var request = "books";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async void fetchBookReadTrack_ReturnsAllBookReadTracks()
+        {
+            // Arrange
+            var request = "bookReadTrack";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async void fetchBookQuote_ReturnsAllBookQuotes()
+        {
+            // Arrange
+            var request = "bookQuote";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+    }
+}
